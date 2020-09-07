a5a5676c3233da15140fccd5a26dad41082bce3f
unknown <jimbaran5601406@gmail.com>
2020-09-07 22:44:16 +0900

    add: truncate関数の追加

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
 //ここに追加したいJavaScript、jQueryを記入してください。
 //このJavaScriptファイルは、親テーマのJavaScriptファイルのあとに呼び出されます。
 //JavaScriptやjQueryで親テーマのjavascript.jsに加えて関数を記入したい時に使用します。
+
+/**
+ *任意の長さで文字列を省略するします。
+ *@param {String} str
+ *@param {Number} len
+ *@return {String}
+ */
+function truncate(str, len) {
+    return str.length <= len ? str : (str.substr(0, len) + "...");
+}
+
