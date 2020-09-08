53bd257e561954384089eea88cd30ebfa33fb0d7
unknown <jimbaran5601406@gmail.com>
2020-09-08 22:41:19 +0900

    modified: タイトル切り捨て開始文字数を45に変更

--- javascript.js
+++ javascript.js
@@ -12,10 +12,10 @@ function truncate(str, len) {
     return str.length <= len ? str : (str.substr(0, len) + "...");
 }
 
-// card-titleをクラス名に持つ要素の文字列を50文字で切り捨てる。
+// card-titleをクラス名に持つ要素の文字列を45文字で切り捨てる。
 (function() {
     let cardsTitle = Array.from(document.getElementsByClassName('card-title'));
     cardsTitle.forEach(cardTitle => {
-        cardTitle.innerHTML = truncate(cardTitle.innerHTML, 50);
+        cardTitle.innerHTML = truncate(cardTitle.innerHTML, 45);
     });
 }());
